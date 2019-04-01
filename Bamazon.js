15d8c4cdeeb85124d6581b85eea141793b253afb
J-Falconetti <joseph.falconetti@gmail.com>
2019-04-01 12:22:19 -0400

    sql update not working

--- Bamazon.js
+++ Bamazon.js
@@ -2,99 +2,110 @@
 // Require npm files mysql and inquire
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+
 //  var Table = require("cli-table");
 
 // Setting connection to SQL db
 var connection = mysql.createConnection({
-    host: "localhost",
-    // setting port/user/password for sql
-    port: 8889,
-    user: "root",
-    password: "root",
-    database: "bamazon_db"
-
+  host: "localhost",
+  // setting port/user/password for sql
+  port: 8889,
+  user: "root",
+  password: "root",
+  database: "bamazon_db"
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    startBam()
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  startBam();
 });
 
 function startBam() {
-    console.log("loading Items")
-    queryAllItems()
+  console.log("loading Items");
+  queryAllItems();
 }
 
 function queryAllItems() {
-    connection.query('SELECT * FROM products', function (err, res) {
-        if (err) throw err;
-        // var displayTable = new Table ({
-        // 	head: ["Item ID", "Product Name", "Price"],
-        //     colWidths: [10,25,14]
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    // var displayTable = new Table ({
+    // 	head: ["Item ID", "Product Name", "Price"],
+    //     colWidths: [10,25,14]
 
-        // });        
-        for (var i = 0; i < res.length; i++) {
-            // displayTable.push([
-            //     res[i].item_id,
-            //     res[i].product_name,
-            //     res[i].price
-            // ]);
-            // console.log(Table.toString());
-            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price + " | ");
+    // });
+    for (var i = 0; i < res.length; i++) {
+      
+      // displayTable.push([
+      //     res[i].item_id,
+      //     res[i].product_name,
+      //     res[i].price
+      // ]);
+      // console.log(Table.toString());
+      console.log(
+        res[i].item_id +
+          " | " +
+          res[i].product_name +
+          " | " +
+          res[i].price +
+          " | "
+      );
 
-
-
-
-            //   console.log("-----------------------------------");
-
-        }
-        purchasePrompt();
-    })
-
+      //   console.log("-----------------------------------");
+    }
+    purchasePrompt();
+  });
 }
 
 function purchasePrompt() {
-    inquirer.prompt([{
-                name: "ID",
-                type: "input",
-                message: "Please enter Item ID you like to purhcase.",
-                filter: Number
-            },
-            {
-                name: "Quantity",
-                type: "input",
-                message: "How many items do you wish to purchase?",
-                filter: Number
+  inquirer
+    .prompt([
+      {
+        name: "ID",
+        type: "input",
+        message: "Please enter Item ID you like to purhcase.",
+        filter: Number
+      },
+      {
+        name: "Quantity",
+        type: "input",
+        message: "How many items do you wish to purchase?",
+        filter: Number
+      }
+    ])
+    .then(function(order) {
+      console.log(order);
+      var item = parseInt(order.ID)
+      // var item = order.ID;
+      console.log("this is the item", item)
+      var qty = order.Quantity;
+      console.log(qty)
+      connection.query(
+        "SELECT * FROM products where item_id=" + item + ";",
+        function(err, res) {
+          if (err) throw err;
+          console.log(res);
 
-            }
-        ])
-        .then(function (order) {
-            var item = order.ID
-            var qty = order.Quantity
-            connection.query('SELECT * FROM products where item_id=' + item + ";", function (err, res) {
-                if (err) throw err;
-                console.log(res)
-
-                if (qty > res[0].stock_quantity) {
-                    console.log("go to Amazon");
-                    startBam()
-                }
-            })
-        })
+          if (qty > res[0].stock_quantity) {
+            console.log("go to Amazon");
+            startBam();
+          } else {
+            total = res[0].price * qty;
+            console.log("Thanks for putting the Bam! in Bamazn");
+            console.log("You owe $" + total);
+            console.log("");
+            //inventory update
+            connection.query("UPDATE products SET ? Where ?", [
+              {
+                stock_quantity: res[0].stock_quantity - qty
+              },
+              {
+                // id: item_id
+              }
+            ]);
+          }
+        }
+      );
+    });
 }
-
-
-
-
-// The app should then prompt users with two messages.
-
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
-// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-// However, if your store does have enough of the product, you should fulfill the customer's order.
-
-// This means updating the SQL database to reflect the remaining quantity.
-// Once the update goes through, show the customer the total cost of their purchase
--- Bamazon2.js
+++ Bamazon2.js
@@ -2,20 +2,20 @@
 // Require npm files mysql and inquire
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var Table = require("cli-table");
+//  var Table = require("cli-table");
 
 // Setting connection to SQL db
 var connection = mysql.createConnection({
     host: "localhost",
-      // setting port/user/password for sql
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "bamazon_db"
+    // setting port/user/password for sql
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "bamazon_db"
 
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     startBam()
@@ -24,55 +24,96 @@ connection.connect(function(err) {
 function startBam() {
     console.log("loading Items")
     queryAllItems()
-
+}
 
 function queryAllItems() {
-    connection.query('SELECT * FROM products', function(err, res){
-        if(err) throw err;  
-        var displayTable = new Table ({
-			head: ["Item ID", "Product Name", "Price"],
-            colWidths: [10,25,14]
-            
-		});        
-      for (var i = 0; i < res.length; i++) {
-        displayTable.push([
-            res[i].item_id,
-            res[i].product_name,
-            res[i].price
-        ]);
-        console.log(Table.toString());
-        // console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price + " | ");
+    connection.query('SELECT * FROM products', function (err, res) {
+        if (err) throw err;
+        // var displayTable = new Table ({
+        // 	head: ["Item ID", "Product Name", "Price"],
+        //     colWidths: [10,25,14]
+
+        // });        
+        for (var i = 0; i < res.length; i++) {
+            // displayTable.push([
+            //     res[i].item_id,
+            //     res[i].product_name,
+            //     res[i].price
+            // ]);
+            // console.log(Table.toString());
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price + " | ");
 
 
-    //   console.log("-----------------------------------");
+
+
+            //   console.log("-----------------------------------");
+
         }
-});
+        purchasePrompt();
+    })
+
 }
-}
-// Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
-// CREATE TABLE products (
-// item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
-// product_name VARCHAR(255),
-// department_name VARCHAR(255),
-// price DECIMAL(10,2) NULL,
-// stock_quantity INTEGER(15),
-// PRIMARY KEY (item_id)
-// );
-//     item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
-//     product_name VARCHAR(255),
-//     department_name VARCHAR(255),
-//     price DECIMAL(10,2) NULL,
-//     stock_quantity INTEGER(15),
-//     PRIMARY KEY (item_id)
-//     );
-// The app should then prompt users with two messages.
 
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
-// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+function purchasePrompt() {
+    inquirer.prompt([{
+                name: "ID",
+                type: "input",
+                message: "Please enter Item ID you like to purhcase.",
+                filter: Number
+            },
+            {
+                name: "Quantity",
+                type: "input",
+                message: "How many items do you wish to purchase?",
+                filter: Number
 
-// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-// However, if your store does have enough of the product, you should fulfill the customer's order.
+            }
+        ])
 
-// This means updating the SQL database to reflect the remaining quantity.
-// Once the update goes through, show the customer the total cost of their purchase.
+        .then(function (order) {
+                var item = order.ID
+                var qty = order.Quantity
+                connection.query('SELECT * FROM products where item_id=' + item + ";", function (err, res) {
+                    if (err) throw err;
+                    console.log(res)
+
+                    if (qty > res[0].stock_quantity) {
+                        console.log("Sorry we do not have enough to fill your order.  Try again ");
+                        startBam()
+
+                            .then(function (order) {
+                                var item = order.ID
+                                var qty = order.Quantity
+                                connection.query('SELECT * FROM products where item_id=' + item + ";", function (err, res) {
+                                    if (err) throw err;
+                                    // console.log(res)
+
+                                    if (qty > res[0].stock_quantity) {
+                                        console.log("Sorry we do not have enough to fill your order.  Try a smaller quanity");
+                                        startBam()
+                                    }
+                                    total = res[0].price * qty;
+                                    currentDepartment = res[0].DepartmentName;
+                                    console.log('Thanks for using BAM!-azon  Your the the best BAM part of Bamazon');
+                                    console.log('You owe $' + total);
+                                    console.log('');
+                                    // update inventory
+                                    connection.query('UPDATE products SET ? Where ?', [{
+                                                stock_quantity: res[0].stock_quantity - order.Quantity
+                                            },
+                                            item_id, item
+
+
+
+                                        ]
+
+                                        , )
+                                    startBam()
+
+                                })
+                            })
+                    }
+
+                })
+            },
+        }
